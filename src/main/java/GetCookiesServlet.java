14f26335610322b233a2448d7ecae7e67e7583f4
Bayakan <spring.boot@hotmail.com>
2024-03-21 09:16:11 +0300

    Куки (Cookies).

---
+++ src/main/java/GetCookiesServlet.java
@@ -0,0 +1,23 @@
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class GetCookiesServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Cookie[] cookies = request.getCookies();
+        PrintWriter printWriter = response.getWriter();
+        printWriter.println("<html>");
+
+        for (Cookie cookie : cookies) {
+            printWriter.println("<h1>" + cookie.getName() + " : " + cookie.getValue() + "</h1>");
+
+        }
+        printWriter.println("</html>");
+    }
+}
